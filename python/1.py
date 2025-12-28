56fbaae392c5bc3d02ae5873446c2e42e7431c9a
Shanjai R <shanjaikannan2008@gmail.com>
2025-12-29 00:39:15 +0530

    Variables of different data types

---
+++ python/1.py
@@ -0,0 +1,10 @@
+age = 20                 # integer
+height = 5.8             # float
+name = "Shanjai"         # string
+are_you_student = True   # boolean
+
+print("---- My Details ----")
+print(f"Name   : {name}")
+print(f"Age    : {age} years")
+print(f"Height : {height} feet")
+print(f"Student: {are_you_student}")
