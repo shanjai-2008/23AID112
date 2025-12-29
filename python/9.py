8bf09466b1baf3e74526a6d04c719e72c66905c1
Shanjai R <shanjaikannan2008@gmail.com>
2025-12-29 08:38:06 +0530

    Exam result

---
+++ python/9.py
@@ -0,0 +1,6 @@
+exam1=int(input("enter the scores 1:"))
+exam2=int(input("enter the scores 2:"))
+if exam1 >= 50 and exam2 >= 50:
+    print("You passed!")
+else:
+    print("You need to retake some exams")
